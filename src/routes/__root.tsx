86b626f68c8b92dc0a15979eab8b3599779a8ea9
Sam <y@mac167deMacBook-Pro.local>
2026-08-12 19:25:14 +0800

    Replace favicon with YOMI brand logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,7 +91,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      { rel: "icon", href: "/favicon.png", type: "image/png", sizes: "32x32" },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "apple-touch-icon", href: "/favicon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
